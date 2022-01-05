264ea1d0af26b8bd6ec72bcb051a5334f559f7c2
Satyam <satyam@satyam.com.ar>
2022-01-05 10:17:55 +0100

    Generate title based on location.pathname. Encapsulate nabVar and listVendedores into static classes

--- public/index.js
+++ public/index.js
@@ -4,17 +4,25 @@ const W = window;
 const H = W.history;
 
 // Helpers
+
+const titles = {
+  '/usuarios': 'Usuarios',
+  '/ventas': 'Ventas',
+  '/vendedores': 'Vendedores',
+  '/distribuidores': 'Distribuidores',
+};
+
 const setTitle = (title) =>
   (document.title = title ? `La Corazón - ${title}` : 'La Corazón');
 
 const router = {
   push: (path, title) => {
     H.pushState({ path }, '', path);
-    setTitle(title);
+    setTitle(title ?? titles[path]);
   },
   replace: (path, title) => {
     H.replaceState({ path }, '', path);
-    setTitle(title);
+    setTitle(title ?? titles[path]);
   },
 };
 
@@ -27,74 +35,99 @@ const apiService = (service, op) =>
     body: JSON.stringify(op),
   });
 // Application state
-let stateVendedores = {
-  loading: true,
-  authenticated: false,
-  error: false,
-  data: null,
-};
-let loadVendedores = () =>
-  apiService('verify')
-    .then((resp) => {
-      stateVendedores.authenticated = resp.ok;
-      if (resp.ok)
-        return apiService('vendedores', {
-          op: 'list',
-        });
-    })
-    .then((resp) => {
-      stateVendedores.error = !resp.ok;
-      if (resp && resp.ok) return resp.json();
-    })
-    .then((data) => {
-      stateVendedores.data = data ?? null;
-      stateVendedores.loading = false;
-    })
-    .catch(() => {
-      stateVendedores.loading = false;
-      stateVendedores.error = true;
-      stateVendedores.data = null;
+const navBar = (() => {
+  let $navItemActive = null;
+  const $navbarMenu = D.getElementById('navbarMenu');
+
+  const onNavbarMenu = (ev) => {
+    ev.preventDefault();
+    const navItem = ev.target.closest('.nav-item');
+    const { path } = navItem.dataset;
+    if (path === W.location.pathname) return;
+
+    $navItemActive?.classList.remove('active');
+    $navItemActive = navItem;
+    $navItemActive.classList.add('active');
+
+    router.push(path);
+  };
+
+  $navbarMenu.onclick = onNavbarMenu;
+})();
+
+const listVendedores = (() => {
+  const $listVendedores = D.getElementById('listVendedores');
+  const $tableVendedores = D.getElementById('tableVendedores');
+  const $tbodyVendedores = $tableVendedores.getElementsByTagName('tbody')[0];
+  const $tplVendedores = D.getElementById('tplVendedores');
+
+  const state = {
+    loading: true,
+    authenticated: false,
+    error: false,
+    data: null,
+  };
+
+  const loadVendedores = () =>
+    apiService('verify')
+      .then((resp) => {
+        state.authenticated = resp.ok;
+        if (resp.ok)
+          return apiService('vendedores', {
+            op: 'list',
+          });
+      })
+      .then((resp) => {
+        state.error = !resp.ok;
+        if (resp && resp.ok) return resp.json();
+      })
+      .then((data) => {
+        state.data = data ?? null;
+        state.loading = false;
+      })
+      .catch(() => {
+        state.loading = false;
+        state.error = true;
+        state.data = null;
+      });
+
+  const render = () => {
+    $listVendedores.classList.remove('hidden');
+    loadVendedores().then(() => {
+      state.data.forEach((v) => {
+        const $row = $tplVendedores.content.cloneNode(true).firstElementChild;
+        $row.dataset.id = v.id;
+        $row.querySelector('.nombre').textContent = v.nombre;
+        $row.querySelector('.email').textContent = v.email;
+
+        $tbodyVendedores.append($row);
+      });
     });
+  };
+  const hide = () => {
+    $listVendedores.classList.add('hidden');
+  };
+
+  return {
+    getState: () => state,
+    loadVendedores,
+    render,
+    hide,
+  };
+})();
 
 // State accessor functions (getters and setters)
 
 // DOM node references
-let $navItemActive = null;
-const $navbarMenu = D.getElementById('navbarMenu');
-
-const $listVendedores = D.getElementById('listVendedores');
-const $tableVendedores = D.getElementById('tableVendedores');
-const $tbodyVendedores = $tableVendedores.getElementsByTagName('tbody')[0];
-const $tplVendedores = D.getElementById('tplVendedores');
 
 // DOM update functions
 
 // Event handlers
-const onNavbarMenu = (ev) => {
-  ev.preventDefault();
-  const navItem = ev.target.closest('.nav-item');
-  const { path, title } = navItem.dataset;
-  if (path === W.location.pathname) return;
-
-  $navItemActive?.classList.remove('active');
-  $navItemActive = navItem;
-  $navItemActive.classList.add('active');
-
-  router.push(path, title);
-};
 
 // Event handler bindings
-$navbarMenu.onclick = onNavbarMenu;
 
 // Initial setup
-$listVendedores.classList.remove('hidden');
-loadVendedores().then(() => {
-  stateVendedores.data.forEach((v) => {
-    const $row = $tplVendedores.content.cloneNode(true).firstElementChild;
-    $row.dataset.id = v.id;
-    $row.querySelector('.nombre').textContent = v.nombre;
-    $row.querySelector('.email').textContent = v.email;
 
-    $tbodyVendedores.append($row);
-  });
-});
+setTitle(titles[location.pathname]);
+
+listVendedores.render();
