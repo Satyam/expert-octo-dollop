06a4982f7e8f307888e723d22317b16537abfadc
Satyam <satyam@satyam.com.ar>
2022-01-28 18:34:44 +0100

    Switched from swc to rollup + typescript plugin

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    dir: 'public',
+    format: 'es',
+    sourceMap: true,
+  },
+  plugins: [typescript({ lib: ['es2021', 'dom'] })],
+};
