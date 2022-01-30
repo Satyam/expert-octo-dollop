12c3e4765b47cf884b23323c14e7a172c743f654
Satyam <satyam@satyam.com.ar>
2022-01-30 16:49:49 +0100

    Added a couple of rollop plugins to tidy up project

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
+import html from 'rollup-plugin-html';
+import copy from 'rollup-plugin-copy';
 
 export default {
   input: 'src/index.ts',
@@ -7,5 +9,31 @@ export default {
     format: 'es',
     sourceMap: true,
   },
-  plugins: [typescript({ lib: ['es2021', 'dom', 'dom.iterable'] })],
+  plugins: [
+    typescript({
+      lib: ['es2021', 'dom', 'dom.iterable'],
+      baseUrl: 'src',
+      moduleResolution: 'node',
+    }),
+    html({
+      include: '**/*.html',
+
+      htmlMinifierOptions: {
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        conservativeCollapse: true,
+        minifyJS: true,
+      },
+    }),
+    copy({
+      targets: [
+        { src: 'src/index.html', dest: 'dist/public' },
+        {
+          src: ['assets/fonts/arial.woff', 'assets/fonts/arial.woff2'],
+          dest: 'dist/public/fonts',
+        },
+        { src: 'assets/images/**/*', dest: 'dist/public/images' },
+      ],
+    }),
+  ],
 };
