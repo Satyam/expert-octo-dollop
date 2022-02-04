51ebd6d36c4c2a25c6ef61288c565a3751218775
Satyam <satyam@satyam.com.ar>
2022-02-04 18:18:07 +0100

    Added link from vendedor field in showVenta.

--- src/showVenta.ts
+++ src/showVenta.ts
@@ -1,29 +1,36 @@
 import { getFirstByTag, getFirstByClass, getById, getTarget } from './gets';
 import apiService from './apiService';
-import { show, hide } from './utils';
+import { show, hide, router } from './utils';
 import { setForm } from './form';
 
 export const showVenta: Handler<{ id: ID }> = ($el) => {
   const $showVenta = $el || getById('showVenta');
-
-  const render = ({ id }) => {
-    apiService<{}, Venta>('ventas', {
-      op: 'get',
-      id,
-    }).then((v) => {
-      if (v) {
-        setForm(getFirstByTag($showVenta, 'form'), {
-          ...v,
-          precioTotal: v.cantidad * v.precioUnitario,
-        });
-        show($showVenta);
-      }
-    });
-  };
+  const $vendedorLink = getFirstByTag($showVenta, 'a');
+  let _listener: EventListener;
+  getFirstByTag($showVenta, 'a').addEventListener('click', (ev) => {});
 
   return {
-    render,
+    render: ({ id }) => {
+      apiService<{}, Venta>('ventas', {
+        op: 'get',
+        id,
+      }).then((v) => {
+        if (v) {
+          _listener = (ev) => {
+            ev.preventDefault();
+            router.push(`/vendedor/${v.idVendedor}`);
+          };
+          $vendedorLink.addEventListener('click', _listener);
+          setForm(getFirstByTag($showVenta, 'form'), {
+            ...v,
+            precioTotal: (v.cantidad || 0) * (v.precioUnitario || 0),
+          });
+          show($showVenta);
+        }
+      });
+    },
     close: () => {
+      if (_listener) $vendedorLink.removeEventListener('click', _listener);
       hide($showVenta);
     },
   };
